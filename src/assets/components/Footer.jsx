35a7ce7bf48124951e521fc700a4c3769d4fe006
Willka <Willka@Mac.lan>
2024-12-01 23:58:17 +0100

    fix

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,7 +3,10 @@ const Footer = () => {
     <footer>
       <div className="footer">
         <div className="footer-img">
-          <img src="src/assets/imgs/marvel_logo.png" alt="logo" />
+          <img
+            src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1582097342/react-new-exercices/Marvel/langfr-1920px-MarvelLogo.svg_uw9pi8.png"
+            alt="logo"
+          />
         </div>
         <p>
           Made at <a href="https://www.lereacteur.io/">Le Reacteur</a> by{" "}
