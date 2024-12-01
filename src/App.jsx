c15a33e2e08dcb5377339e0122cfb41e7e35e299
Willka <Willka@Mac.lan>
2024-12-01 23:14:07 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Characters from "./assets/pages/Characters";
 import Comics from "./assets/pages/Comics";
 import Favorites from "./assets/pages/Favorites";
 import Header from "./assets/components/Header";
 import CharacterComics from "./assets/pages/CharacterComics";
-import Signup from "./assets/pages/SignUp";
+import Signup from "./assets/pages/Signup";
 import Login from "./assets/pages/Login";
 import Cookies from "js-cookie";
 import Footer from "./assets/components/Footer";
